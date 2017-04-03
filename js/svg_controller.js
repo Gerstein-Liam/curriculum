5047726c79717fc2ac4b193f89fbf5f0059df364
DESKTOP-E3R823T\liamg <liam.gerstein@gmail.com>
2017-04-03 21:39:40 +0200

    Mise en page de l'application " logical gate sim"

--- js/svg_controller.js
+++ js/svg_controller.js
@@ -7,7 +7,8 @@ var time_stamp;
 mainApp.controller('SVG_Ctrl', function($scope) {
     $scope.count = 0;
     time_stamp = 0;
-    var svgContainer = d3.select("#draw_zone").append("svg").attr("width", 800).attr("height", 800);
+    var svgContainer = d3.select("#draw_zone").append("svg").attr("width", 200).attr("height", 400);
+	//var svgContainer = d3.select("#draw_zone").append("svg").attr("width", 800).attr("height", 800);
     $scope.myFunction = function() {
         var signal_1 = Boolean($scope.signal_1);
         var signal_2 = Boolean($scope.signal_2);
@@ -21,7 +22,7 @@ mainApp.controller('SVG_Ctrl', function($scope) {
         $scope.count = signal_res;
         var circle = svgContainer.append("polyline").attr("points", state2).attr("stroke", "blue").attr("stroke-width", 3);
         $scope.count = signal_res;
-        var circle = svgContainer.append("polyline").attr("points", "0,135,800,135").attr("stroke", "black").attr("stroke-width", 1);
+        var circle = svgContainer.append("polyline").attr("points", "0,135,350,135").attr("stroke", "black").attr("stroke-width", 1);
         $scope.count = signal_res;
         var circle = svgContainer.append("polyline").attr("points", stateres).attr("stroke", "green").attr("stroke-width", 3);
         $scope.count = signal_res;
