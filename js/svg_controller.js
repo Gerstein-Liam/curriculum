757add10aa21b8f99bc694abde006b0088af0674
DESKTOP-E3R823T\liamg <liam.gerstein@gmail.com>
2017-04-17 23:19:28 +0200

    Logical sim: Nettoyage du code

--- js/svg_controller.js
+++ js/svg_controller.js
@@ -1,29 +1,31 @@
 
 mainApp.controller('SVG_Ctrl', function($scope) {
     
+	$scope.i_green_state = 0;
+	$scope.i_blue_state = 0;
 	
 	
+	
+	var green_state=0;
+	var blue_state=0;
 	var past_state=0;
 	var current_state=0;
+	var time_stamp = 0;
 	
 	
-	
-	
-    var time_stamp = 0;
     var svgContainer = d3.select("#draw_zone").append("svg").attr("width", 200).attr("height", 400);
 	//var svgContainer = d3.select("#draw_zone").append("svg").attr("width", 800).attr("height", 800);
     
-	 $scope.signal_1 = 0;
-	 $scope.signal_2 = 0;
+	
      
 	
 	
 	$scope.myFunction = function() {
-        var signal_1 = Boolean($scope.signal_1);
-        var signal_2 = Boolean($scope.signal_2);
-        var current_state = signal_1 & signal_2;
-        var state1 = "" + String(time_stamp) + "," + String(10 + !signal_1 * 50) + "," + String(time_stamp + 30) + "," + String(10 + !signal_1 * 50) + "";
-        var state2 = "" + String(time_stamp) + "," + String(70 + !signal_2 * 50) + "," + String(time_stamp + 30) + "," + String(70 + !signal_2 * 50) + "";
+        green_state = Boolean($scope.i_green_state);
+        blue_state = Boolean($scope.i_blue_state);
+        current_state = green_state & blue_state;
+        var state1 = "" + String(time_stamp) + "," + String(10 + !green_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !green_state * 50) + "";
+        var state2 = "" + String(time_stamp) + "," + String(70 + !blue_state * 50) + "," + String(time_stamp + 30) + "," + String(70 + !blue_state * 50) + "";
         var stateres = "" + String(time_stamp) + "," + String(140 + !current_state * 50) + "," + String(time_stamp + 30) + "," + String(140 + !current_state * 50) + "";
         time_stamp = time_stamp + 30;
         $scope.past_state = current_state; 
