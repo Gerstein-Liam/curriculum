0334e9e49d89b60817fe1598bd667df81089577c
DESKTOP-E3R823T\liamg <liam.gerstein@gmail.com>
2017-04-17 23:01:04 +0200

    Logical Sim: Ajout de l'état passé dans panneau de controle

--- js/svg_controller.js
+++ js/svg_controller.js
@@ -1,12 +1,15 @@
 
-var holder;
-var last_state;
-var time_stamp;
-
-
 mainApp.controller('SVG_Ctrl', function($scope) {
-    $scope.count = 0;
-    time_stamp = 0;
+    
+	
+	
+	var past_state=0;
+	var current_state=0;
+	
+	
+	
+	
+    var time_stamp = 0;
     var svgContainer = d3.select("#draw_zone").append("svg").attr("width", 200).attr("height", 400);
 	//var svgContainer = d3.select("#draw_zone").append("svg").attr("width", 800).attr("height", 800);
     
@@ -18,19 +21,19 @@ mainApp.controller('SVG_Ctrl', function($scope) {
 	$scope.myFunction = function() {
         var signal_1 = Boolean($scope.signal_1);
         var signal_2 = Boolean($scope.signal_2);
-        var signal_res = signal_1 & signal_2;
+        var current_state = signal_1 & signal_2;
         var state1 = "" + String(time_stamp) + "," + String(10 + !signal_1 * 50) + "," + String(time_stamp + 30) + "," + String(10 + !signal_1 * 50) + "";
         var state2 = "" + String(time_stamp) + "," + String(70 + !signal_2 * 50) + "," + String(time_stamp + 30) + "," + String(70 + !signal_2 * 50) + "";
-        var stateres = "" + String(time_stamp) + "," + String(140 + !signal_res * 50) + "," + String(time_stamp + 30) + "," + String(140 + !signal_res * 50) + "";
+        var stateres = "" + String(time_stamp) + "," + String(140 + !current_state * 50) + "," + String(time_stamp + 30) + "," + String(140 + !current_state * 50) + "";
         time_stamp = time_stamp + 30;
-       
+        $scope.past_state = current_state; 
         var circle = svgContainer.append("polyline").attr("points", state1).attr("stroke", "blue").attr("stroke-width", 3);
-        $scope.count = signal_res;
+        $scope.count = current_state;
         var circle = svgContainer.append("polyline").attr("points", state2).attr("stroke", "blue").attr("stroke-width", 3);
-        $scope.count = signal_res;
+        $scope.count = current_state;
         var circle = svgContainer.append("polyline").attr("points", "0,135,350,135").attr("stroke", "black").attr("stroke-width", 1);
-        $scope.count = signal_res;
+        $scope.count = current_state;
         var circle = svgContainer.append("polyline").attr("points", stateres).attr("stroke", "green").attr("stroke-width", 3);
-        $scope.count = signal_res;
+        $scope.count = current_state;
     }
 });
