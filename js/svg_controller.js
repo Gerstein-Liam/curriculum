89de0a2b9297a73c8259f1d6e0d663d2749c660c
DESKTOP-E3R823T\liamg <liam.gerstein@gmail.com>
2017-04-17 23:48:32 +0200

    Logical sim: Renommage variable "state" avec ajout prefixe " current" et "past"

--- js/svg_controller.js
+++ js/svg_controller.js
@@ -6,10 +6,14 @@ mainApp.controller('SVG_Ctrl', function($scope) {
 	
 	
 	
-	var green_state=0;
-	var blue_state=0;
+	var current_green_state=0;
+	var current_blue_state=0;
+	var current_res_red_state=0;
 	
-	var res_red_state=0;
+	var past_green_state=0;
+	var past_blue_state=0;
+	var past_res_red_state=0;
+	
 	var past_state=0;
 	var time_stamp = 0;
 	
@@ -27,15 +31,15 @@ mainApp.controller('SVG_Ctrl', function($scope) {
 	
 	
 	$scope.myFunction = function() {
-        green_state = Boolean($scope.i_green_state);
-        blue_state = Boolean($scope.i_blue_state);
-        res_red_state = green_state & blue_state;
+        current_green_state = Boolean($scope.i_green_state);
+        current_blue_state = Boolean($scope.i_blue_state);
+        current_res_red_state = current_green_state & current_blue_state;
 		
-		svg_green_state = "" + String(time_stamp) + "," + String(10 + !green_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !green_state * 50) + "";
-        svg_blue_state = "" + String(time_stamp) + "," + String(10 + !blue_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !blue_state * 50) + "";
-		svg_res_red_state = "" + String(time_stamp) + "," + String(10 + !res_red_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !res_red_state * 50) + "";
+		svg_green_state = "" + String(time_stamp) + "," + String(10 + !current_green_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_green_state * 50) + "";
+        svg_blue_state = "" + String(time_stamp) + "," + String(10 + !current_blue_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_blue_state * 50) + "";
+		svg_res_red_state = "" + String(time_stamp) + "," + String(10 + !current_res_red_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_res_red_state * 50) + "";
         time_stamp = time_stamp + 30;
-        $scope.o_res_red_past_state = res_red_state; 
+        $scope.o_res_red_past_state = current_res_red_state; 
 		
 		svgGreenZone.append("polyline").attr("points", svg_green_state).attr("stroke", "green").attr("stroke-width", 3);
         svgBlueZone.append("polyline").attr("points", svg_blue_state).attr("stroke", "blue").attr("stroke-width", 3);
