3d6a345332c5efd19a450f25836be39fad42e63d
DESKTOP-E3R823T\liamg <liam.gerstein@gmail.com>
2017-04-17 23:27:25 +0200

    Logical Sim: Nettoyage du code 2

--- js/svg_controller.js
+++ js/svg_controller.js
@@ -8,9 +8,14 @@ mainApp.controller('SVG_Ctrl', function($scope) {
 	
 	var green_state=0;
 	var blue_state=0;
+	
+	var res_red_state=0;
 	var past_state=0;
-	var current_state=0;
 	var time_stamp = 0;
+	
+	var svg_green_state ;
+	var svg_blue_state ;
+	 var svg_res_red_state;
 	
 	
     var svgContainer = d3.select("#draw_zone").append("svg").attr("width", 200).attr("height", 400);
@@ -23,19 +28,18 @@ mainApp.controller('SVG_Ctrl', function($scope) {
 	$scope.myFunction = function() {
         green_state = Boolean($scope.i_green_state);
         blue_state = Boolean($scope.i_blue_state);
-        current_state = green_state & blue_state;
-        var state1 = "" + String(time_stamp) + "," + String(10 + !green_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !green_state * 50) + "";
-        var state2 = "" + String(time_stamp) + "," + String(70 + !blue_state * 50) + "," + String(time_stamp + 30) + "," + String(70 + !blue_state * 50) + "";
-        var stateres = "" + String(time_stamp) + "," + String(140 + !current_state * 50) + "," + String(time_stamp + 30) + "," + String(140 + !current_state * 50) + "";
+        res_red_state = green_state & blue_state;
+		
+		svg_green_state = "" + String(time_stamp) + "," + String(10 + !green_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !green_state * 50) + "";
+        svg_blue_state = "" + String(time_stamp) + "," + String(70 + !blue_state * 50) + "," + String(time_stamp + 30) + "," + String(70 + !blue_state * 50) + "";
+		svg_res_red_state = "" + String(time_stamp) + "," + String(140 + !res_red_state * 50) + "," + String(time_stamp + 30) + "," + String(140 + !res_red_state * 50) + "";
         time_stamp = time_stamp + 30;
-        $scope.past_state = current_state; 
-        var circle = svgContainer.append("polyline").attr("points", state1).attr("stroke", "blue").attr("stroke-width", 3);
-        $scope.count = current_state;
-        var circle = svgContainer.append("polyline").attr("points", state2).attr("stroke", "blue").attr("stroke-width", 3);
-        $scope.count = current_state;
-        var circle = svgContainer.append("polyline").attr("points", "0,135,350,135").attr("stroke", "black").attr("stroke-width", 1);
-        $scope.count = current_state;
-        var circle = svgContainer.append("polyline").attr("points", stateres).attr("stroke", "green").attr("stroke-width", 3);
-        $scope.count = current_state;
+        $scope.o_res_red_past_state = res_red_state; 
+		
+		svgContainer.append("polyline").attr("points", svg_green_state).attr("stroke", "green").attr("stroke-width", 3);
+        svgContainer.append("polyline").attr("points", svg_blue_state).attr("stroke", "blue").attr("stroke-width", 3);
+        svgContainer.append("polyline").attr("points", "0,135,350,135").attr("stroke", "black").attr("stroke-width", 1);
+        svgContainer.append("polyline").attr("points", svg_res_red_state).attr("stroke", "red").attr("stroke-width", 3);
+     
     }
 });
