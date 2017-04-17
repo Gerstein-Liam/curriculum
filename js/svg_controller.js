c100060483f818f0ce531f4a1a452d113f7367f6
DESKTOP-E3R823T\liamg <liam.gerstein@gmail.com>
2017-04-17 23:42:43 +0200

    Logical gate: Decoupage de zone 1

--- js/svg_controller.js
+++ js/svg_controller.js
@@ -17,7 +17,8 @@ mainApp.controller('SVG_Ctrl', function($scope) {
 	var svg_blue_state ;
 	 var svg_res_red_state;
 	
-	
+	 var svgGreenZone = d3.select("#green_zone").append("svg").attr("width", 200).attr("height", 100);$
+	 var svgBlueZone = d3.select("#blue_zone").append("svg").attr("width", 200).attr("height", 100);
     var svgContainer = d3.select("#draw_zone").append("svg").attr("width", 200).attr("height", 400);
 	//var svgContainer = d3.select("#draw_zone").append("svg").attr("width", 800).attr("height", 800);
     
@@ -31,13 +32,13 @@ mainApp.controller('SVG_Ctrl', function($scope) {
         res_red_state = green_state & blue_state;
 		
 		svg_green_state = "" + String(time_stamp) + "," + String(10 + !green_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !green_state * 50) + "";
-        svg_blue_state = "" + String(time_stamp) + "," + String(70 + !blue_state * 50) + "," + String(time_stamp + 30) + "," + String(70 + !blue_state * 50) + "";
+        svg_blue_state = "" + String(time_stamp) + "," + String(10 + !blue_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !blue_state * 50) + "";
 		svg_res_red_state = "" + String(time_stamp) + "," + String(140 + !res_red_state * 50) + "," + String(time_stamp + 30) + "," + String(140 + !res_red_state * 50) + "";
         time_stamp = time_stamp + 30;
         $scope.o_res_red_past_state = res_red_state; 
 		
-		svgContainer.append("polyline").attr("points", svg_green_state).attr("stroke", "green").attr("stroke-width", 3);
-        svgContainer.append("polyline").attr("points", svg_blue_state).attr("stroke", "blue").attr("stroke-width", 3);
+		svgGreenZone.append("polyline").attr("points", svg_green_state).attr("stroke", "green").attr("stroke-width", 3);
+        svgBlueZone.append("polyline").attr("points", svg_blue_state).attr("stroke", "blue").attr("stroke-width", 3);
         svgContainer.append("polyline").attr("points", "0,135,350,135").attr("stroke", "black").attr("stroke-width", 1);
         svgContainer.append("polyline").attr("points", svg_res_red_state).attr("stroke", "red").attr("stroke-width", 3);
      
