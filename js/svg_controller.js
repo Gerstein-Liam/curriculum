7c6d60b05df56a8472515d077bc6b430c194351c
DESKTOP-E3R823T\liamg <liam.gerstein@gmail.com>
2017-04-18 19:56:36 +0200

    Logical sim: Ajout des flancs

--- js/svg_controller.js
+++ js/svg_controller.js
@@ -35,15 +35,64 @@ mainApp.controller('SVG_Ctrl', function($scope) {
         current_blue_state = Boolean($scope.i_blue_state);
         current_res_red_state = current_green_state & current_blue_state;
 		
-		svg_green_state = "" + String(time_stamp) + "," + String(10 + !current_green_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_green_state * 50) + "";
-        svg_blue_state = "" + String(time_stamp) + "," + String(10 + !current_blue_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_blue_state * 50) + "";
-		svg_res_red_state = "" + String(time_stamp) + "," + String(10 + !current_res_red_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_res_red_state * 50) + "";
+		if(current_green_state!=past_green_state){
+			if (current_green_state==0){
+			svg_green_state = ""+String(time_stamp)+"," +String(10 + 0 * 50)+"," + String(time_stamp) + "," + String(10 + !current_green_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_green_state * 50) + "";	
+			}
+			else{
+			svg_green_state = ""+String(time_stamp)+"," +String(10 + 1 * 50)+"," + String(time_stamp) + "," + String(10 + !current_green_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_green_state * 50) + "";	
+			}}
+		else{
+			svg_green_state = "" + String(time_stamp) + "," + String(10 + !current_green_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_green_state * 50) + "";
+			}
+			
+			
+			
+		if(current_blue_state!=past_blue_state){
+			if (current_blue_state==0){
+			svg_blue_state = ""+String(time_stamp)+"," +String(10 + 0 * 50)+"," + String(time_stamp) + "," + String(10 + !current_blue_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_blue_state * 50) + "";	
+			}
+			else{
+			svg_blue_state = ""+String(time_stamp)+"," +String(10 + 1 * 50)+"," + String(time_stamp) + "," + String(10 + !current_blue_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_blue_state * 50) + "";	
+			}}
+		else{
+			svg_blue_state = "" + String(time_stamp) + "," + String(10 + !current_blue_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_blue_state * 50) + "";
+			}
+			
+			
+		if(current_res_red_state!=past_res_red_state){
+			if (current_res_red_state==0){
+			svg_res_red_state = ""+String(time_stamp)+"," +String(10 + 0 * 50)+"," + String(time_stamp) + "," + String(10 + !current_res_red_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_res_red_state * 50) + "";	
+			}
+			else{
+			svg_res_red_state = ""+String(time_stamp)+"," +String(10 + 1 * 50)+"," + String(time_stamp) + "," + String(10 + !current_res_red_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_res_red_state * 50) + "";	
+			}}
+		else{
+			svg_res_red_state = "" + String(time_stamp) + "," + String(10 + !current_res_red_state * 50) + "," + String(time_stamp + 30) + "," + String(10 + !current_res_red_state * 50) + "";
+			}
+		
+		
+        
+		
         time_stamp = time_stamp + 30;
         $scope.o_res_red_past_state = current_res_red_state; 
 		
-		svgGreenZone.append("polyline").attr("points", svg_green_state).attr("stroke", "green").attr("stroke-width", 3);
-        svgBlueZone.append("polyline").attr("points", svg_blue_state).attr("stroke", "blue").attr("stroke-width", 3);
-		svgRedZone.append("polyline").attr("points", svg_res_red_state).attr("stroke", "red").attr("stroke-width", 3);
+		svgGreenZone.append("polyline").attr("points", svg_green_state).style("fill", "none").attr("stroke", "green").attr("stroke-width", 3);
+        svgBlueZone.append("polyline").attr("points", svg_blue_state).style("fill", "none").attr("stroke", "blue").attr("stroke-width", 3);
+		svgRedZone.append("polyline").attr("points", svg_res_red_state).style("fill", "none").attr("stroke", "red").attr("stroke-width", 3);
+		
+		
+		past_green_state=current_green_state;
+		past_blue_state=current_blue_state;
+		past_res_red_state=current_res_red_state;
+		
+		
      
     }
 });
+
+
+holder.append("polyline")      // attach a polyline
+    .style("stroke", "black")  // colour the line
+    .style("fill", "none")     // remove any fill colour
+    .attr("points", "100,50, 200,150, 300,50");  // x,y points
